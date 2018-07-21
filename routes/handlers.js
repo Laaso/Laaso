b594d179ae201ac2cb07c6814c62ff4c24938423
Julia Lynn Rogers <julia@xetanai.moe>
2018-07-21 00:53:57 -0400

    eslint fixes

--- routes/handlers.js
+++ routes/handlers.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const laasocfg = require('../config/laaso');
 
 router.get('*', (req,res) => {
-    console.log(`404 ERROR: ${req.originalUrl}`)
+    console.log(`404 ERROR: ${req.originalUrl}`);
     return res.status(404).altRender('error_404');
 });
 
